6c3dc7201959c12be05f9d6749b777b6d0a70768
Retrofoxx <retrofoxx@protonmail.com>
2022-05-26 00:51:24 -0400

    about

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+    return (
+        <main style={{ height: '100%' }}>
+            <Layout>
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage
